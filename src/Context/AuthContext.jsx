b4323bc27eb8d1aef687f230812f73bf05785bed
zenaaj <jaredzena@gmail.com>
2025-12-07 18:09:38 +0300

    Update AuthContext.jsx

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
